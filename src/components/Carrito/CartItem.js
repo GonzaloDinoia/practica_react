6de0180ed26ba637536e95ee3f4b8980f697d754
Gonzalo Dinoia <gonzalodinoia@gmail.com>
2022-09-09 22:57:00 -0300

    correcciones

--- src/components/Carrito/CartItem.js
+++ src/components/Carrito/CartItem.js
@@ -1,35 +1,64 @@
-const CartItem = ({data,deleteFromCart,addToCart}) => {
-    const {id,img,title,author,price,quantity}=data;
-    let subtotal=quantity*price
-    
-    return (
-    <div className="w-full h-48 p-4 m-1.5 rounded-lg overflow-hidden bg-white shadow-md cursor-pointer hover:shadow-lg">
-    <div className="flex flex-row items-center justify-around w-full h-full">
-    <img  className="w-30 m-4 h-40" src={img} alt="libro"></img>
-    <div className="flex flex-col items-center p-4">
-    <h2 className="font-ftitles text-xl font-bold text-center mb-4">{title}</h2>
-    <p className="font-ftext text-center">{author}</p>
-    </div>
-    <div className="flex flex-col items-center p-4">
-    <h2 className="font-ftitles text-xl font-bold text-center mb-4">Precio Unitario</h2>
-    <p className="font-ftext text-center">${price}</p>
-    </div>
-    <div className="flex flex-col items-center p-4">
-    <h2 className="font-ftitles text-xl font-bold text-center mb-4">Cantidad</h2>
-    <div className="flex">
-    <button onClick={() => deleteFromCart(id)} >-</button>
-    <p className="font-ftext text-center">{quantity}</p>
-    <button onClick={() => addToCart(id)} >+</button>
-    </div>
-    </div>
-    <div className="flex flex-col items-center p-4">
-    <h2 className="font-ftitles text-xl font-bold text-center mb-4">Subtotal</h2>
-    <p className="font-ftext text-center">${subtotal}</p>
-    </div>
-    <button className="text-2xl" onClick={() => deleteFromCart(id, true)}><ion-icon name="trash-outline"></ion-icon></button>
-    </div>
-    </div>
+//item carrito
+const CartItem = ({ data, deleteFromCart, addToCart }) => {
+  const { id, img, title, author, price, quantity } = data;
+  let subtotal = quantity * price;
 
-    )
-    }
-    export default CartItem
+  return (
+    <div className="flex flex-row justify-around w-full h-48 py-2 px-2  m-1.5 rounded-lg overflow-hidden bg-white shadow-md cursor-pointer hover:shadow-lg">
+      <div className="w-full h-full flex  items-center justify-around rounded-lg flex-row border-2 p-2">
+        <div className=" h-full flex flex-none items-center justify-center">
+          <img className="w-30 h-40" src={img} alt="libro"></img>
+        </div>
+
+        <div className=" h-full  flex  items-start justify-around flex-col md:ml-4 md:items-center md:flex-row md:w-full md:justify-between">
+          <div className="flex flex-col items-center justify-center md:w-60 md:items-start ">
+            <h2 className="font-ftitles text-xl font-bold mb-1">{title}</h2>
+            <p className="font-ftext">{author}</p>
+          </div>
+          <div className="hidden md:flex flex-col items-center justify-center md:flex-none">
+            <h2 className="font-ftitles text-xl font-bold mb-1">
+              Precio Unitario
+            </h2>
+            <p className="font-ftext">${price}</p>
+          </div>
+          <div className="flex flex-col items-center justify-start md:mr-20">
+            <h2 className="hidden md:flex font-ftitles text-xl font-bold mb-2">
+              Cantidad
+            </h2>
+            <div className="flex items-center">
+              <button
+                onClick={() => deleteFromCart(id)}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold px-2.5 rounded-l"
+              >
+                -
+              </button>
+              <p className="p-1 font-ftext text-xl text-center">{quantity}</p>
+              <button
+                onClick={() => addToCart(id)}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold px-2 rounded-r"
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-full flex flex-col items-center justify-between md:flex-row-reverse">
+          <div className="md:ml-12">
+            <button
+              className=" text-3xl hover:text-red-800 hover:scale-125"
+              onClick={() => deleteFromCart(id, true)}
+            >
+              <ion-icon name="trash-outline"></ion-icon>
+            </button>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="font-ftitles text-xl font-bold mb-1">Subtotal</h2>
+            <p className="font-ftext">${subtotal}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CartItem;
--- src/components/NavBar/BtnCart.js
+++ src/components/NavBar/BtnCart.js
@@ -1,16 +1,22 @@
-import {useConsumer} from "../cartContextProviders"
+import { useConsumer } from "../cartContextProviders";
 const BtnCart = () => {
   const [state] = useConsumer();
-   
-  const cartItemNumber=state.cart.reduce((count, curItem) => {
+
+  const cartItemNumber = state.cart.reduce((count, curItem) => {
     return count + curItem.quantity;
-  }, 0)
+  }, 0);
 
   return (
     <>
-    <button className=" relative flex justify-center w-10 h-10 p-1 ml-8 text-3xl font-bold text-gray-800 align-middle duration-500 border-2 border-gray-800 border-solid rounded-full cursor-pointer hover:text-gray-400 hover:border-gray-400 "><ion-icon name="cart-outline"></ion-icon></button>
-    <div className="inline-flex absolute top-2 right-8 justify-center items-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full border-2 border-white dark:border-gray-900">{cartItemNumber}</div>
+      <button className="md:relative md:flex md:justify-center w-10 h-10 p-1 ml-8 text-3xl font-bold text-gray-800 align-middle duration-500 border-2 border-gray-800 border-solid rounded-full cursor-pointer hover:text-gray-400 hover:border-gray-400 ">
+        <ion-icon name="cart-outline"></ion-icon>
+      </button>
+      {state.cart.length > 0 && (
+        <div className="inline-flex md:absolute top-2 right-8  justify-center items-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full border-2 border-white dark:border-gray-900">
+          {cartItemNumber}
+        </div>
+      )}
     </>
-  )
-}  
-export default BtnCart
+  );
+};
+export default BtnCart;
